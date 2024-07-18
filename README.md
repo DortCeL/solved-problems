dd4160d15d255b7c3d006ee4bdd9b3f3dd86ad92
DortCeL <exh.alif.47@gmail.com>
2024-07-18 10:29:20 +0600

    hehe

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # solved-problems
+
 here i keep the interesting problems that i have solved from time to time. These solutions have potential to be improved and can be used to solve different problems.
+
+- Leetcode
---
+++ word_search.js
@@ -0,0 +1,69 @@
+// searching given word from a board...
+// ? plan : show the position of the characters chosen somehow...
+
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+
+const board = [
+	["A", "B", "C", "E"],
+	["S", "F", "C", "S"],
+	["A", "D", "E", "E"],
+];
+const word = "FDA";
+
+const print = () => {
+	for (let i = 0; i < board.length; i++) {
+		console.log(board[i]);
+	}
+};
+
+var exist = function (board, word) {
+	const m = board.length;
+	const n = board[0].length;
+
+	let dfs = (row, col, flag) => {
+		print();
+		// base true case
+		if (flag >= word.length) return true;
+
+		console.log(`flag = ${flag}`);
+
+		// base false case
+		if (
+			row >= m ||
+			col >= n ||
+			row < 0 ||
+			col < 0 ||
+			board[row][col] !== word[flag]
+		)
+			return false;
+
+		const backup = board[row][col];
+		board[row][col] = "*";
+
+		if (
+			dfs(row + 1, col, flag + 1) ||
+			dfs(row, col + 1, flag + 1) ||
+			dfs(row - 1, col, flag + 1) ||
+			dfs(row, col - 1, flag + 1)
+		) {
+			return true;
+		}
+		board[row][col] = backup;
+		return false;
+	};
+
+	for (let i = 0; i < m; i++) {
+		for (let j = 0; j < n; j++) {
+			console.log(`i=${i}, j=${j}`);
+			if (dfs(i, j, 0)) return true;
+		}
+	}
+
+	return false;
+};
+
+console.log(exist(board, word));
